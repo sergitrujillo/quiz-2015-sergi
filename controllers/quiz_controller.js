92cb62242ce10599d9183bccd7ad52ca88a85bc2
Sergi Trujillo <sergi.trujillo@gmail.com>
2015-07-03 11:52:19 +0200

    Sistema de autoload de quizId

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,6 +1,21 @@
 
 var models = require('../models/models.js');
 
+exports.load = function(req,res,next,quizId){
+	models.Quiz.findById(req.params.quizId).then(
+		function(quiz){
+			if (quiz) {
+				req.quiz = quiz;
+				next();
+			}else{
+				next(new Error('No existe quizId='+quizId));
+			}
+
+			res.render('quizes/show',{quiz:quiz});
+		}
+	).catch(function(error){next(error);})
+}
+
 exports.index = function(req,res){
 	models.Quiz.findAll().then(function(quizes){
 		res.render("quizes/index",{quizes:quizes});
@@ -9,9 +24,7 @@ exports.index = function(req,res){
 
 
 exports.show = function(req,res){
-	models.Quiz.findById(req.params.quizId).then(function(quiz){
-		res.render('quizes/show',{quiz:quiz});
-	})
+	res.render('quizes/show',{quiz:req.quiz});
 };
 /*
 exports.question = function(req,res){
@@ -22,13 +35,11 @@ exports.question = function(req,res){
 };
 */
 exports.answer = function(req,res){
-	models.Quiz.findById(req.params.quizId).then(function(quiz) {
-		if (req.query.respuesta === quiz.respuesta){
-			res.render('quizes/answer',{quiz:quiz, respuesta:'Correcto'});
-		}else{
-			res.render('quizes/answer',{quiz:quiz, respuesta:'Incorrecto'});
-		}	
-	});
+	var resultado = 'Incorrecto';
+	if (req.query.respuesta === req.quiz.respuesta){
+		resultado = 'Correcto';
+	}
+	res.render('quizes/answer',{quiz:req.quiz, respuesta:resultado});
 };
 
 exports.author = function(req,res){
--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,9 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+// Autoload de comandes con :quizId
+router.param('quizId',quizController.load); // autoload de quizId
+
 router.get('/quizes',quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show)
 //router.get('/quizes/question',quizController.question);
