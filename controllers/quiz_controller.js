13673fa70c3ee1f489d23d1cb11a6d5ceb844649
Sergi Trujillo <sergi.trujillo@gmail.com>
2015-07-03 11:46:42 +0200

    Añadida opcion de multiples preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,19 +1,32 @@
 
 var models = require('../models/models.js');
 
+exports.index = function(req,res){
+	models.Quiz.findAll().then(function(quizes){
+		res.render("quizes/index",{quizes:quizes});
+	})
+}
+
+
+exports.show = function(req,res){
+	models.Quiz.findById(req.params.quizId).then(function(quiz){
+		res.render('quizes/show',{quiz:quiz});
+	})
+};
+/*
 exports.question = function(req,res){
 	models.Quiz.findAll().then(function(quiz){
 		res.render('quizes/question',{pregunta:quiz[0].pregunta});	
 	})
 	
 };
-
+*/
 exports.answer = function(req,res){
-	models.Quiz.findAll().then(function(quiz) {
-		if (req.query.respuesta === quiz[0].respuesta){
-			res.render('quizes/answer',{respuesta:'Correcto'});
+	models.Quiz.findById(req.params.quizId).then(function(quiz) {
+		if (req.query.respuesta === quiz.respuesta){
+			res.render('quizes/answer',{quiz:quiz, respuesta:'Correcto'});
 		}else{
-			res.render('quizes/answer',{respuesta:'Incorrecto'});
+			res.render('quizes/answer',{quiz:quiz, respuesta:'Incorrecto'});
 		}	
 	});
 };
