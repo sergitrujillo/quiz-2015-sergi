2e69c2c15556f68895c30bf6aafdddb1fe156553
Sergi Trujillo <sergi.trujillo@gmail.com>
2015-07-03 10:33:58 +0200

    BBDD postgres en heroku SQLite en localhost

--- models/models.js
+++ models/models.js
@@ -1,12 +1,30 @@
 var path = require('path');
 
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_NAME = (url[6]||null);
+var user = (url[2]||null);
+var pwd = (url[3]||null);
+var protocol = (url[1]||null);
+var dialect = (url[1]||null);
+var port = (url[5]||null);
+var host = (url[4]||null);
+var storage = process.env.DATABASE_STORAGE;
 
 // Cargar Modelo ORM
 var Sequelize = require('sequelize');
 
 // Usar BBDD SQLite:
-var sequelize = new Sequelize(null,null,null,
-	{ dialect:"sqlite",storage:"quiz.sqlite"});
+var sequelize = new Sequelize(DB_NAME,user,pwd,
+	{ 	dialect: 	protocol, 
+		protocol: 	protocol, 
+		port: 		port, 
+		host: 		host,
+		storage: 	storage, // solo SQLite 
+		omitNull: 	true //solo Postgres
+	});
+
+
+
 
 // Importar a definicion de la tabla Quiz en quiz.js
 var Quiz = sequelize.import(path.join(__dirname,'quiz'));
