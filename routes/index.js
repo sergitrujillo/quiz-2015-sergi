c072a8611e5147c94132c4356eeb7a5bbbf9eadf
Sergi Trujillo <sergi.trujillo@gmail.com>
2015-07-12 14:02:25 +0200

    Añadido sistema de Autorización de gestion de las preguntas

--- routes/index.js
+++ routes/index.js
@@ -16,17 +16,17 @@ router.param('quizId',quizController.load); // autoload de quizId
 // Definicion de rutas de sesion
 router.get('/login', sessionController.new);
 router.post('/login', sessionController.create);
-router.get('/logout',sessionController.destroy);
+router.get('/logout', sessionController.destroy);
 
 // Definicion de rutas de /quizes
 router.get('/quizes/:quizId(\\d+)', quizController.show)
 //router.get('/quizes/question',quizController.question);
 router.get('/quizes/:quizId(\\d+)/answer',quizController.answer);
-router.get('/quizes/new',quizController.new);
-router.post('/quizes/create',quizController.create);
-router.get('/quizes/:quizId(\\d+)/edit',quizController.edit);
-router.put('/quizes/:quizId(\\d+)', quizController.update);
-router.delete('/quizes/:quizId(\\d+)', quizController.destroy);
+router.get('/quizes/new'				,sessionController.loginRequired, quizController.new);
+router.post('/quizes/create'			,sessionController.loginRequired, quizController.create);
+router.get('/quizes/:quizId(\\d+)/edit'	,sessionController.loginRequired, quizController.edit);
+router.put('/quizes/:quizId(\\d+)'	 	,sessionController.loginRequired, quizController.update);
+router.delete('/quizes/:quizId(\\d+)' 	,sessionController.loginRequired, quizController.destroy);
 router.get('/quizes',quizController.index);
 
 // Definicion de rutas de comentarios
